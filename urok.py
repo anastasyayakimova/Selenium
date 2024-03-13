80dc23aab3eb5b84f2707cb1b52f33c9c25535c8
anastasyayakimova <110032979+anastasyayakimova@users.noreply.github.com>
2024-03-13 13:27:32 +0300

    Update urok.py

--- urok.py
+++ urok.py
@@ -1,11 +1,20 @@
 import time
-
+import random
+from random import choice, sample
+import string
 from selenium import webdriver
 from selenium.common import NoSuchElementException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.common.keys import Keys
+
+domains = [ "hotmail.com", "gmail.com", "aol.com", "mail.com" , "mail.kz", "yahoo.com"]
+letters = string.ascii_lowercase[:12]
+def get_random_domain(domains): return random.choice(domains)
+def get_random_name(letters, length): return ''.join(random.choice(letters) for i in range(length))
+def generate_random_emails(nb, length): return [get_random_name(letters, length) + '@' + get_random_domain(domains) for i in range(nb)]
+def generate_mail(): return (generate_random_emails(1, 7))
 
 brauser=webdriver.Chrome()
 brauser.implicitly_wait(10)
@@ -20,7 +29,9 @@ City=brauser.find_element(By.CSS_SELECTOR, "input[name='city']").send_keys("Voro
 sel=brauser.find_element(By.CSS_SELECTOR,"span.select2-selection__arrow").click()
 element=brauser.find_element(By.CSS_SELECTOR,"input.select2-search__field").send_keys("Algeria" + Keys.ENTER)
 
-Email=brauser.find_element(By.CSS_SELECTOR, "input[name='email']").send_keys("yakiiiim@gmail.com")
+email=generate_mail()
+Email=brauser.find_element(By.CSS_SELECTOR, "input[name='email']").send_keys(email)
+
 Phone=brauser.find_element(By.CSS_SELECTOR,"input[name='phone']").send_keys("876543567")
 D_passord=brauser.find_element(By.CSS_SELECTOR,"input[name='password']").send_keys("12345678")
 C_password=brauser.find_element(By.CSS_SELECTOR,"input[name='confirmed_password']").send_keys("12345678")
@@ -28,7 +39,7 @@ create_account=brauser.find_element(By.CSS_SELECTOR,"button[name='create_account
 
 logout=brauser.find_element(By.LINK_TEXT,"Logout").click()
 
-login_email=brauser.find_element(By.CSS_SELECTOR,"input[name='email']").send_keys('yakiiiim@gmail.com')
+login_email=brauser.find_element(By.CSS_SELECTOR,"input[name='email']").send_keys(email)
 login_password=brauser.find_element(By.CSS_SELECTOR,"input[name='password']").send_keys('12345678')
 login=brauser.find_element(By.CSS_SELECTOR,"button[name='login']").click()
 brauser.find_element(By.LINK_TEXT,"Logout").click()
